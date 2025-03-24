4b2a683278174e6029b2cce395cc29da44c460d2
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-03-24 14:42:55 +0100

    Add project files.

---
+++ AlphaWebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaWebApp.Controllers;
+
+public class ProjectsController : Controller
+{
+    [Route("projects")]
+    public IActionResult Projects()
+    {
+        return View();
+    }
+}
