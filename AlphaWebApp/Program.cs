38c0a016a781c27c1ebf5f975a30ab58f357f01a
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-04-02 17:03:38 +0200

    Chore(ProjectFactory): Add dependency in program.cs

--- AlphaWebApp/Program.cs
+++ AlphaWebApp/Program.cs
@@ -1,10 +1,12 @@
 using Data.Contexts;
+using Business.Factories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ProjectFactory>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
