a8c4ecb86c6db420953fe730c73a9c0caf4038f1
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-04-02 18:18:53 +0200

    Feat(Controller): Add get and post methods for projects

---
+++ AlphaWebApp/Controllers/ProjectController.cs
@@ -0,0 +1,35 @@
+﻿using AlphaWebApp.Business.Models;
+using Data.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaWebApp.Controllers;
+[Route("projects")]
+public class ProjectController(AppDbContext context) : Controller
+{
+    private readonly AppDbContext _context = context;
+
+    [Route("")]
+    public IActionResult Projects()
+    {
+        return View();
+    }
+
+    [HttpGet("create")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> Create(ProjectViewModel project)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Projects");
+        }
+        return View(project);
+    }
+
+}
