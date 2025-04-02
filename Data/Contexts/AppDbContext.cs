6dae4ea94e190313ff4b1be28e9c428a2d6cbd9b
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-04-02 12:30:44 +0200

    Structure(Data): Create local db

---
+++ Data/Contexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts;
+
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<ProjectEntity> Projects { get; set; }
+}
+
---
+++ Data/Entities/ProjectEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class ProjectEntity
+    {
+        public int Id { get; set; }
+
+        public string ProjectName { get; set; } = null!;
+
+        public string ClientName { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Budget { get; set; }
+
+        public string Status { get; set; } = null!;
+    }
+}
