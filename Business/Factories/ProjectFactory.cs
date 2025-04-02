e4da8022009e51a273a13eb2c0d0a61d83529489
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-04-02 16:59:42 +0200

    Factory(Project): Create factory for mapping model/entity

---
+++ Business/Factories/ProjectFactory.cs
@@ -0,0 +1,38 @@
+﻿using Data.Entities;
+using AlphaWebApp.Business.Models;
+
+
+namespace Business.Factories;
+
+//Mappa entitet till viewmodel och viewmodel till entitet.
+
+public class ProjectFactory
+{
+    public ProjectEntity Create(ProjectViewModel model)
+    {
+        return new ProjectEntity
+        {
+            ProjectName = model.ProjectName,
+            ClientName = model.ClientName,
+            Description = model.Description,
+            StartDate = model.StartDate,
+            EndDate = model.EndDate,
+            Budget = model.Budget,
+            Status = model.Status
+        };
+    }
+
+    public ProjectViewModel Create(ProjectEntity entity)
+    {
+        return new ProjectViewModel
+        {
+            ProjectName = entity.ProjectName,
+            ClientName = entity.ClientName,
+            Description = entity.Description,
+            StartDate = entity.StartDate,
+            EndDate = entity.EndDate,
+            Budget = entity.Budget,
+            Status = entity.Status
+        };
+    }
+}
