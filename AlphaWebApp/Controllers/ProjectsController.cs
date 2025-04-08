1be5cc930af431227ec1060abe52c4de7771b28f
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-04-08 09:26:41 +0200

    Rename(ProjectController): To ProjectsController to fix routing issue

---
+++ AlphaWebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,39 @@
+﻿using AlphaWebApp.Business.Models;
+using Business.Factories;
+using Data.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaWebApp.Controllers;
+[Route("projects")]
+public class ProjectsController(AppDbContext context, ProjectFactory projectFactory) : Controller
+{
+    private readonly AppDbContext _context = context;
+    private readonly ProjectFactory _projectFactory = projectFactory;
+
+    [Route("")]
+    public IActionResult Projects()
+    {
+        return View();
+    }
+
+    [HttpGet("create")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> Create(ProjectViewModel project)
+    {
+        if (ModelState.IsValid)
+        {
+            var entity = _projectFactory.Create(project);
+            _context.Projects.Add(entity);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Projects");
+        }
+        return View(project);
+    }
+
+}
