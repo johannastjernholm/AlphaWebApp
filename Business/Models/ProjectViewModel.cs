f874a452740bbc7b7caee5c801b3785d12298422
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-04-08 10:18:02 +0200

    Refactor(ProjectViewModel): Add validation

--- Business/Models/ProjectViewModel.cs
+++ Business/Models/ProjectViewModel.cs
@@ -1,19 +1,27 @@
-﻿namespace AlphaWebApp.Business.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlphaWebApp.Business.Models;
 
 public class ProjectViewModel
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = " A project name is required.")]
     public string ProjectName { get; set; } = null!;
 
+    [Required(ErrorMessage = "Client name is required.")]
     public string ClientName { get; set; } = null!;
 
+    [Required(ErrorMessage = "Description is required.")]
     public string Description { get; set; } = null!;
 
+    [Required(ErrorMessage = "Start date is required.")]
     public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public DateTime? EndDate { get; set; }
 
+    [Range(0.1, double.MaxValue, ErrorMessage ="Budget must be greater than 0.")]
     public decimal Budget { get; set; }
 
+    [Required(ErrorMessage = "Status is required.")]
     public string Status { get; set; } = null!;
 }
