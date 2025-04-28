d563c057f1849203b3b85a10be840f70fc6247e6
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-04-28 13:31:55 +0200

    Feat(Login): Add method for logging in

--- AlphaWebApp/Controllers/AuthController.cs
+++ AlphaWebApp/Controllers/AuthController.cs
@@ -6,14 +6,15 @@ using Microsoft.AspNetCore.Mvc;
 namespace AlphaWebApp.Controllers;
 
 [Route("auth")]
-public class AuthController(UserManager<ApplicationUser> userManager) : Controller
+public class AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager = userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager = signInManager;
 
     [HttpGet("register")]
     public IActionResult Register()
     {
-       return View();
+        return View();
     }
 
     [HttpPost("register")]
@@ -31,15 +32,46 @@ public class AuthController(UserManager<ApplicationUser> userManager) : Controll
 
         var result = await _userManager.CreateAsync(user, registerViewModel.Password);
 
-        if(result.Succeeded) {
+        if (result.Succeeded)
+        {
             return RedirectToAction("Login");
         }
 
-        foreach(var error in result.Errors)
+        foreach (var error in result.Errors)
         {
+            Console.WriteLine($"Error: { error.Description}");
             ModelState.AddModelError(string.Empty, error.Description);
         }
 
         return View(registerViewModel);
     }
+
+
+    [HttpGet("login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(loginViewModel);
+        }
+
+        var result = await _signInManager.PasswordSignInAsync(loginViewModel.Email, loginViewModel.Password, loginViewModel.RememberMe, lockoutOnFailure: false);
+
+        if (result.Succeeded)
+        {
+            return RedirectToAction("Projects", "Projects");
+        }
+        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+
+        return View(loginViewModel);
+    }
+
+
+    //TODO: Fixa log out
 }
--- AlphaWebApp/Program.cs
+++ AlphaWebApp/Program.cs
@@ -25,6 +25,7 @@ var app = builder.Build();
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapStaticAssets();
 
